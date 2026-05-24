32e74ab773b79e07234b96cfc43f6d0795c242f3
Syed Hyder <hyder@Syeds-MacBook-Air.local>
2026-05-24 13:25:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
+            KeyLo
+        </div>
+    )
+}
