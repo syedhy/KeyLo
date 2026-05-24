dc0d81d85f3a7f5814a634f62f91ff8d022b7df9
Syed Hyder <hyder@Syeds-MacBook-Air.local>
2026-05-25 00:14:07 +0530

    Remove redirect auth service

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,51 +2,28 @@ import { useEffect , useState } from "react"
 import { onAuthStateChanged } from "firebase/auth"
 
 import { auth } from "../firebase/firebaseConfig"
-
 import { AuthContext } from "./authContextValue"
-
-import {
-    ensureUserData ,
-    handleRedirectLogin
-} from "../firebase/authService"
+import { ensureUserData } from "../firebase/authService"
 
 export function AuthProvider({ children }) {
     const [user , setUser] = useState(null)
     const [authLoading , setAuthLoading] = useState(true)
 
     useEffect(() => {
-        async function initAuth() {
-            try {
-                await handleRedirectLogin()
-            } catch (error) {
-                console.error(error)
+        const unsubscribe = onAuthStateChanged(auth , async (currentUser) => {
+            if (currentUser) {
+                await ensureUserData(currentUser)
             }
 
-            const unsubscribe = onAuthStateChanged(
-                auth ,
-                async (currentUser) => {
-                    if (currentUser) {
-                        await ensureUserData(currentUser)
-                    }
+            setUser(currentUser)
+            setAuthLoading(false)
+        })
 
-                    setUser(currentUser)
-                    setAuthLoading(false)
-                }
-            )
-
-            return unsubscribe
-        }
-
-        initAuth()
+        return unsubscribe
     } , [])
 
     return (
-        <AuthContext.Provider
-            value={{
-                user ,
-                authLoading
-            }}
-        >
+        <AuthContext.Provider value={{ user , authLoading }}>
             {children}
         </AuthContext.Provider>
     )
